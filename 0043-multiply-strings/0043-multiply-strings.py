f98516c884970d011748697b6ac3881c511e3b44
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-01-02 04:05:19 +0300

    Time: 33 ms (93.36%), Space: 13.8 MB (98.29%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,11 @@
 class Solution:
+#     def ToInteger(string):
+#         dict = {"1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "0":0}
+#         num
+#         if 
+#         return dict[string]
+       
     def multiply(self, num1: str, num2: str) -> str:
+        # return str(self.ToInteger(num1) * self.ToInteger(num2))
         return str(int(num1) * int(num2))
         
