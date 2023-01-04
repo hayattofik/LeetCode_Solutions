e44c442839abc103ddbfc4bcc4d400ce35ee4e34
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-01-04 18:56:08 +0300

    Time: 9410 ms (5.04%), Space: 21.6 MB (71.53%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,20 @@
+class Solution:
+    # def check_powerof_two(self,num):
+    #     if math.ceil(log2(num)) == math.floor(log2(num)):
+    #         return True
+    #     else:
+    #         return False
+    def countPairs(self, deliciousness: List[int]) -> int:
+        count = 0
+        for power in range(41):
+            checkD=defaultdict(int)
+            
+            target = 2 **power
+            for num in  deliciousness:
+                check = target- num
+                if check in checkD:
+                    count += checkD[check]
+                checkD[num] +=1
+            
+        return count % (10**9 + 7)
+                    
