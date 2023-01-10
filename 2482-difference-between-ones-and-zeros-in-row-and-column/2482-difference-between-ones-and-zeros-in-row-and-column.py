2a7db7b348bf166a11f6e6776907665c840245de
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-01-10 16:10:57 +0300

    Time: 1885 ms (79.95%), Space: 59.8 MB (10.96%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,23 +1,25 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        onesRow=defaultdict(int)
+        onesRow = defaultdict(int)
         onesColumn = defaultdict(int)
         output=[]
         
+        #make reference for rows and columns with 1 value
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == 1:
                     onesRow[row] += 1
                     onesColumn[col] +=1
-        
-        for i in range(len(grid)):
-            temp=[]
-            for j in range(len(grid[0])):
-                
-                diff = onesRow[i] + onesColumn[j] - (len(grid[0])-onesRow[i]) - (len(grid)-onesColumn[j])
-               
-                temp.append(diff)
-            output.append(temp)
+                    
+        #calculate the difference 
+        for row in range(len(grid)):
+            diffRow=[]
+            for col in range(len(grid[0])):
+                diff = onesRow[row] + onesColumn[col] - (len(grid[0])-onesRow[row]) - (len(grid)-onesColumn[col])
+                diffRow.append(diff)
+            output.append(diffRow)
+            
+            
         return output
                 
                 
