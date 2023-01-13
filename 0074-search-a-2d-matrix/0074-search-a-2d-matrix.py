282292a59a51c6151bbb539b4f92e8eaa2e95b40
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-01-13 19:21:46 +0300

    Time: 41 ms (93.95%), Space: 14.5 MB (40.80%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,8 +3,8 @@ class Solution:
         col = len(matrix[0])
         row = len(matrix)
         
-        for col in matrix:
-            col = set(col)
+        # for col in matrix:
+        #     col = set(col)
         
         for row in matrix:
             if row[-1] >= target and target in row:
