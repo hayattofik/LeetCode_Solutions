b122b711a915c5d46b1b5e4f673d2483016854ca
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-02-28 18:30:05 +0300

    Time: 76 ms (69.02%), Space: 13.9 MB (59.79%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cnt = Counter(s1)
+        
+        l = 0
+        for r, c in enumerate(s2):
+            cnt[c] -= 1
+            while cnt[c] < 0:  
+                cnt[s2[l]] += 1  
+                l += 1
+            if r - l + 1 == len(s1):  
+                return True
+            
+        return False
+        
+        
