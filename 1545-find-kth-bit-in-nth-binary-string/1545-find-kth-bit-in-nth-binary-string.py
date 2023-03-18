2271e5a093f47efa5814bc43da868336efdb0eca
Hayat Tofik <96552730+hayattofik@users.noreply.github.com>
2023-03-18 22:12:41 +0300

    Time: 35 ms (79.85%), Space: 14 MB (74.66%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,6 +1,6 @@
 class Solution:
 	def findKthBit(self, n: int, k: int) -> str:
-		# print(n,k)
+		
 		if(n == 1):
 			return '0'
 		mid = (2**n)//2 
@@ -9,7 +9,7 @@ class Solution:
 		if(k < mid):
 			return self.findKthBit(n-1,k)
 		else:
-			# return opp of val since k is greater than mid
+			
 			val = self.findKthBit(n-1,(2**n) - k)
 			if(val == '0'):
 				return '1'
